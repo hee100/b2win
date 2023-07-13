23e288299be3410d333e029d97681dfaf9f9d571
hee100 <ggng200@naver.com>
2023-07-13 15:24:15 +0900

    refactor : api 주소 변경

--- src/main/java/com/b2win/projectbowl/member/controller/MemberController.java
+++ src/main/java/com/b2win/projectbowl/member/controller/MemberController.java
@@ -7,10 +7,7 @@ import com.b2win.projectbowl.utils.UriCreator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -23,7 +20,7 @@ public class MemberController {
     private final static String MEMBER_DEFAULT_URL = "/members";
     private final MemberService memberService;
 
-    @PostMapping("/members")
+    @PostMapping("/member/save")
     public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post requestBody) {
         Member member = Member.fromDto(requestBody);
         Member createdMember = memberService.createMember(member);
